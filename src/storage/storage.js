5e515030c4db6772e9372d347fa3434391afb5c1
boyra <b9892213@gmail.com>
2025-04-21 00:58:33 +0300

    added all paths '/' at start

--- src/storage/storage.js
+++ src/storage/storage.js
@@ -1,23 +1,23 @@
-import clearSvg from "src/assets/img/icons/forecast-indicators/clear.svg";
-import cloudsSvg from "src/assets/img/icons/forecast-indicators/clouds.svg";
-import drizzleSvg from "src/assets/img/icons/forecast-indicators/drizzle.svg";
-import feelsLikeSvg from "src/assets/img/icons/forecast-indicators/feels-like.svg";
-import rainSvg from "src/assets/img/icons/forecast-indicators/rain.svg";
-import snowSvg from "src/assets/img/icons/forecast-indicators/snow.svg";
-import thunderstormSvg from "src/assets/img/icons/forecast-indicators/thunderstorm.svg";
-import temperatureSvg from "src/assets/img/icons/forecast-indicators/temperature.svg";
-import waterSvg from "src/assets/img/icons/forecast-indicators/water.svg";
+import clearSvg from "/src/assets/img/icons/forecast-indicators/clear.svg";
+import cloudsSvg from "/src/assets/img/icons/forecast-indicators/clouds.svg";
+import drizzleSvg from "/src/assets/img/icons/forecast-indicators/drizzle.svg";
+import feelsLikeSvg from "/src/assets/img/icons/forecast-indicators/feels-like.svg";
+import rainSvg from "/src/assets/img/icons/forecast-indicators/rain.svg";
+import snowSvg from "/src/assets/img/icons/forecast-indicators/snow.svg";
+import thunderstormSvg from "/src/assets/img/icons/forecast-indicators/thunderstorm.svg";
+import temperatureSvg from "/src/assets/img/icons/forecast-indicators/temperature.svg";
+import waterSvg from "/src/assets/img/icons/forecast-indicators/water.svg";
 
-import bigClearSvg from "src/assets/img/icons/big-forecast-indicators/clear.svg";
-import bigCloudsSvg from "src/assets/img/icons/big-forecast-indicators/clouds.svg";
-import bigDrizzleSvg from "src/assets/img/icons/big-forecast-indicators/drizzle.svg";
-import bigRainSvg from "src/assets/img/icons/big-forecast-indicators/rain.svg";
-import bigSnowSvg from "src/assets/img/icons/big-forecast-indicators/snow.svg";
-import bigThunderstormSvg from "src/assets/img/icons/big-forecast-indicators/thunderstorm.svg";
+import bigClearSvg from "/src/assets/img/icons/big-forecast-indicators/clear.svg";
+import bigCloudsSvg from "/src/assets/img/icons/big-forecast-indicators/clouds.svg";
+import bigDrizzleSvg from "/src/assets/img/icons/big-forecast-indicators/drizzle.svg";
+import bigRainSvg from "/src/assets/img/icons/big-forecast-indicators/rain.svg";
+import bigSnowSvg from "/src/assets/img/icons/big-forecast-indicators/snow.svg";
+import bigThunderstormSvg from "/src/assets/img/icons/big-forecast-indicators/thunderstorm.svg";
 
-import arrowLeftSvg from "src/assets/img/icons/common-icons/arrow-left.svg";
-import locationCurrentSvg from "src/assets/img/icons/common-icons/location-current.svg";
-import magnifyingGlassSvg from "src/assets/img/icons/common-icons/magnifying-glass.svg";
+import arrowLeftSvg from "/src/assets/img/icons/common-icons/arrow-left.svg";
+import locationCurrentSvg from "/src/assets/img/icons/common-icons/location-current.svg";
+import magnifyingGlassSvg from "/src/assets/img/icons/common-icons/magnifying-glass.svg";
 
 export const indicators = {
   clear: clearSvg,
